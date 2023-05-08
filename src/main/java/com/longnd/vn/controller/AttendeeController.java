dcb3c24ccd85bb597ffdee7e6047655f16d23d39
duogglong <long22262@gmail.com>
2023-05-08 23:09:41 +0700

    basic api

--- src/main/java/com/longnd/vn/controller/AttendeeController.java
+++ src/main/java/com/longnd/vn/controller/AttendeeController.java
@@ -3,10 +3,7 @@ package com.longnd.vn.controller;
 import com.longnd.vn.entity.AttendeeEntity;
 import com.longnd.vn.repository.AttendeeRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,8 +20,33 @@ public class AttendeeController {
 
     @GetMapping("/all")
     public List<AttendeeEntity> getAll() {
-        log.info("Call getAll");
+        log.info("Call getAll()");
         List<AttendeeEntity> result = attendeeRepository.findAll();
         return result;
+    }
+
+    @GetMapping("/{id}")
+    public AttendeeEntity getById(@PathVariable("id") Long id) {
+        log.info("Call getById()");
+        return attendeeRepository.findById(id).get();
+    }
+
+    @PutMapping
+    public AttendeeEntity update(@RequestBody AttendeeEntity attendee) {
+        log.info("Call update()");
+        return attendeeRepository.save(attendee);
+    }
+
+    @PostMapping
+    public AttendeeEntity create(@RequestBody AttendeeEntity attendee) {
+        log.info("Call create()");
+        return attendeeRepository.save(attendee);
+    }
+
+    @DeleteMapping("/{id}")
+    public Boolean deleteById(@PathVariable("id") Long id) {
+        log.info("Call deleteById()");
+        attendeeRepository.deleteById(id);
+        return true;
     }
 }
