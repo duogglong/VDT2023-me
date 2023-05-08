b3a96c233bc9306ed7cb4eb59f57bf5115556a8a
duogglong <long22262@gmail.com>
2023-05-08 12:49:09 +0700

    initial be

---
+++ src/main/java/com/longnd/vn/controller/AttendeeController.java
@@ -0,0 +1,23 @@
+package com.longnd.vn.controller;
+
+//import com.longnd.hw1.entity.AttendeeEntity;
+//import com.longnd.hw1.repository.AttendeeRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/api/attendees")
+public class AttendeeController {
+//    private final AttendeeRepository attendeeRepository;
+//
+//    public AttendeeController(AttendeeRepository attendeeRepository) {
+//        this.attendeeRepository = attendeeRepository;
+//    }
+//    @GetMapping
+//    public String showAttendees(Model model) {
+//        List<AttendeeEntity> attendees = attendeeRepository.findAll();
+////        List<AttendeeEntity> attendees = new ArrayList<>();
+//        model.addAttribute("attendees", attendees);
+//        return "filter-all-attendees";
+//    }
+}
