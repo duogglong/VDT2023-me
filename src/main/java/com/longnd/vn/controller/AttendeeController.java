92829c20eab837e8257123f7eddee316a1898dc5
duogglong <long22262@gmail.com>
2023-05-08 15:27:44 +0700

    connect db

--- src/main/java/com/longnd/vn/controller/AttendeeController.java
+++ src/main/java/com/longnd/vn/controller/AttendeeController.java
@@ -1,23 +1,30 @@
 package com.longnd.vn.controller;
 
-//import com.longnd.hw1.entity.AttendeeEntity;
-//import com.longnd.hw1.repository.AttendeeRepository;
-import org.springframework.stereotype.Controller;
+import com.longnd.vn.entity.AttendeeEntity;
+import com.longnd.vn.repository.AttendeeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping(value = "/api/attendees")
+@CrossOrigin("*")
+@Slf4j
 public class AttendeeController {
-//    private final AttendeeRepository attendeeRepository;
-//
-//    public AttendeeController(AttendeeRepository attendeeRepository) {
-//        this.attendeeRepository = attendeeRepository;
-//    }
-//    @GetMapping
-//    public String showAttendees(Model model) {
-//        List<AttendeeEntity> attendees = attendeeRepository.findAll();
-////        List<AttendeeEntity> attendees = new ArrayList<>();
-//        model.addAttribute("attendees", attendees);
-//        return "filter-all-attendees";
-//    }
+    private final AttendeeRepository attendeeRepository;
+
+    public AttendeeController(AttendeeRepository attendeeRepository) {
+        this.attendeeRepository = attendeeRepository;
+    }
+
+    @GetMapping("/all")
+    public List<AttendeeEntity> getAll() {
+        log.info("Call getAll");
+        List<AttendeeEntity> result = attendeeRepository.findAll();
+        return result;
+    }
 }
--- src/main/java/com/longnd/vn/repository/AttendeeRepository.java
+++ src/main/java/com/longnd/vn/repository/AttendeeRepository.java
@@ -1,7 +1,7 @@
-//package com.longnd.hw1.repository;
-//
-//import com.longnd.hw1.entity.AttendeeEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface AttendeeRepository extends JpaRepository<AttendeeEntity, Long> {
-//}
+package com.longnd.vn.repository;
+
+import com.longnd.vn.entity.AttendeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AttendeeRepository extends JpaRepository<AttendeeEntity, Long> {
+}
